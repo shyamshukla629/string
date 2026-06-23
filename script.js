f4aa519bbad7fd245409b4c69e0f2c9f943c2212
Aditya Shukla <shyamshukla9578@gmail.com>
2026-06-23 16:32:34 +0530

    We are learing git

---
+++ script.js
@@ -0,0 +1,14 @@
+// stirng
+
+// let str="ADITYASHUKLA";
+// console.log(str);
+// console.log(str.length);
+// console.log(str[0]);
+
+//  template literal
+let obj={
+    item:"Pen",
+    price:"10",
+};
+let output=`The cost of ${obj.item} is ${obj.price} rupees`;
+console.log(output);
