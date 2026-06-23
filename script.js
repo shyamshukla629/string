83231cfc88e45da5a0cd639ef117baeb2c295512
Aditya Shukla <shyamshukla9578@gmail.com>
2026-06-23 16:50:37 +0530

    We are learing string in javascript

--- script.js
+++ script.js
@@ -6,9 +6,20 @@
 // console.log(str[0]);
 
 //  template literal
-let obj={
-    item:"Pen",
-    price:"10",
-};
-let output=`The cost of ${obj.item} is ${obj.price} rupees`;
-console.log(output);
+// let obj={
+//     item:"Pen",
+//     price:"10",
+// };
+// let output=`The cost of ${obj.item} is ${obj.price} rupees`;
+// console.log(output);
+
+// String method 
+// let str ="AdityaShukla";
+// str=str.toUpperCase();
+// console.log(str);
+// str=str.toLowerCase();
+// console.log(str);
+
+// trim method 
+ let str="     Aditya Shukla Javascript       ";
+ console.log(str.trim());
